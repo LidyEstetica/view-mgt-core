1ee344a6486ff55a7ee62388d098777f6208c64d
yIorran <iorran.gv@hotmail.com>
2025-03-17 19:36:15 -0300

    chore: commit inicial

--- src/main/java/com/lidy/estetica/repository/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lidy.estetica.repository;
-
-import com.lidy.estetica.model.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
-}
--- src/main/java/com/lidy/estetica/repository/ProcedimentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lidy.estetica.repository;
-
-import com.lidy.estetica.model.Procedimento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProcedimentoRepository extends JpaRepository<Procedimento, Integer> {
-}
